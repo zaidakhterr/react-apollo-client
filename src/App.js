ef334aa1360079eaf4365ebb42c0c7d59f71158a
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-02 00:44:49 +0500

    📦 NEW: Fetch data with client.query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { gql } from "@apollo/client";
 import React from "react";
+import { useEffect } from "react";
+import client from "./apolloClient";
 
 function App() {
+  useEffect(() => {
+    client
+      .query({
+        query: gql`
+          query getAuthors {
+            authors(pageNo: 1) {
+              name
+            }
+          }
+        `,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  }, []);
   return (
     <div className="app">
       <h1>Hello World</h1>
