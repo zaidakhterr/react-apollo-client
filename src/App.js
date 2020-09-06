552753c88304e95b9dd8500514dc6de542dbc1b9
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-06 12:12:06 +0500

    📦 NEW: Authors page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Notfound from "./pages/404";
+import Authors from "./pages/Authors";
 import Home from "./pages/Home";
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <div className="md:container mx-auto px-4 min-h-screen flex flex-col justify-center align-middle">
         <Switch>
           <Route exact path="/" children={<Home />} />
-          <Route exact path="/authors" children={<h1>Authors</h1>} />
+          <Route exact path="/authors" children={<Authors />} />
           <Route exact path="/books" children={<h1>Book</h1>} />
           <Route exact path="*" children={<Notfound />} />
         </Switch>
---
+++ src/pages/Authors.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const Authors = () => {
+  const { data, loading, error } = useQuery(
+    gql`
+      query getAuthors {
+        authors {
+          name
+          email
+        }
+      }
+    `
+  );
+
+  return (
+    <div className="min-h-screen py-10">
+      <h1 className="text-center text-4xl font-bold mb-8">Authors</h1>
+      {loading && <div className="text-center">Loading</div>}
+      {error && <div className="text-center">Error</div>}
+      {data && (
+        <div>
+          {data?.authors?.map((author) => (
+            <div className="border-2 rounded py-4 px-8 mb-2 flex flex-col sm:flex-row sm:justify-between">
+              <h4 className="text-lg">{author.name}</h4>
+              <p className="text-gray-600 text-sm">{author.email}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Authors;
