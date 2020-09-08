aa10a9eb8aaf49a568b0b8ec9ad5fb1f9341d030
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-09 02:19:56 +0500

    📦 NEW: Paginate Authors page

--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -1,13 +1,22 @@
-import React from "react";
-import { useQuery } from "@apollo/client";
+import React, { useEffect, useState } from "react";
+import { useQuery, makeVar } from "@apollo/client";
 import { GET_AUTHORS } from "../graphql/queryGetAuthor";
 
 const Authors = () => {
-  const { data, loading, error } = useQuery(GET_AUTHORS, {
+  const [pageNo, setPageNo] = useState(1);
+  const { data, loading, error, fetchMore } = useQuery(GET_AUTHORS, {
     variables: {
-      pageNo: 1,
+      pageNo,
     },
   });
+
+  useEffect(() => {
+    fetchMore({
+      variables: {
+        pageNo,
+      },
+    });
+  }, [pageNo, fetchMore]);
 
   return (
     <div className="min-h-screen py-10">
@@ -27,14 +36,34 @@ const Authors = () => {
         </div>
       )}
       {data && (
-        <div>
-          {data?.authors?.list.map((author) => (
-            <div className="border-2 rounded py-4 px-8 mb-2 flex flex-col sm:flex-row sm:justify-between">
-              <h4 className="text-lg">{author.name}</h4>
-              <p className="text-gray-600 text-sm">{author.email}</p>
-            </div>
-          ))}
-        </div>
+        <>
+          <div>
+            {data?.authors?.list.map((author) => (
+              <div className="border-2 rounded py-4 px-8 mb-2 flex flex-col sm:flex-row sm:justify-between">
+                <h4 className="text-lg">{author.name}</h4>
+                <p className="text-gray-600 text-sm">{author.email}</p>
+              </div>
+            ))}
+          </div>
+          <div className="mt-5">
+            {pageNo !== 1 && (
+              <button
+                className="float-left text-center inline-block bg-green-400 hover:bg-green-500 text-white py-1 px-4 rounded transition-colors duration-300 ease-in-out"
+                onClick={() => setPageNo((n) => n - 1)}
+              >
+                PREV
+              </button>
+            )}
+            {data?.authors?.hasMore && (
+              <button
+                className="float-right text-center inline-block bg-green-400 hover:bg-green-500 text-white py-1 px-4 rounded transition-colors duration-300 ease-in-out"
+                onClick={() => setPageNo((n) => n + 1)}
+              >
+                NEXT
+              </button>
+            )}
+          </div>
+        </>
       )}
     </div>
   );
