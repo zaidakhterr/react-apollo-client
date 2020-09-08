a82655e40ad584504baf60a7c24fabf345fd4db8
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-09 02:03:35 +0500

    👌 IMPROVE: add variables and emojis

--- src/graphql/queryGetAuthor.js
+++ src/graphql/queryGetAuthor.js
@@ -1,10 +1,13 @@
 const { gql } = require("@apollo/client");
 
 export const GET_AUTHORS = gql`
-  query getAuthors {
-    authors {
-      name
-      email
+  query getAuthors($pageNo: Int!) {
+    authors(pageNo: $pageNo) {
+      list {
+        name
+        email
+      }
+      hasMore
     }
   }
 `;
--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -3,16 +3,32 @@ import { useQuery } from "@apollo/client";
 import { GET_AUTHORS } from "../graphql/queryGetAuthor";
 
 const Authors = () => {
-  const { data, loading, error } = useQuery(GET_AUTHORS);
+  const { data, loading, error } = useQuery(GET_AUTHORS, {
+    variables: {
+      pageNo: 1,
+    },
+  });
 
   return (
     <div className="min-h-screen py-10">
       <h1 className="text-center text-4xl font-bold mb-8">Authors</h1>
-      {loading && <div className="text-center">Loading</div>}
-      {error && <div className="text-center">Error</div>}
+      {loading && (
+        <div className="text-center">
+          <span role="img" aria-label="loading">
+            🔄
+          </span>
+        </div>
+      )}
+      {error && (
+        <div className="text-center">
+          <span role="img" aria-label="error">
+            💔
+          </span>
+        </div>
+      )}
       {data && (
         <div>
-          {data?.authors?.map((author) => (
+          {data?.authors?.list.map((author) => (
             <div className="border-2 rounded py-4 px-8 mb-2 flex flex-col sm:flex-row sm:justify-between">
               <h4 className="text-lg">{author.name}</h4>
               <p className="text-gray-600 text-sm">{author.email}</p>
