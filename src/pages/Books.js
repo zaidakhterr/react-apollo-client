a76b84b5f9029d767c6eeaf60302a069a89a7449
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-09 02:33:16 +0500

    📦 NEW: Complete Books page

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -1,9 +1,70 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_BOOKS } from "../graphql/queryGetBooks";
 
 const Books = () => {
+  const [pageNo, setPageNo] = useState(1);
+  const { data, loading, error, fetchMore } = useQuery(GET_BOOKS, {
+    variables: {
+      pageNo,
+    },
+  });
+
+  useEffect(() => {
+    fetchMore({
+      variables: {
+        pageNo,
+      },
+    });
+  }, [pageNo, fetchMore]);
+
   return (
-    <div>
-      <h1>Books</h1>
+    <div className="min-h-screen py-10">
+      <h1 className="text-center text-4xl font-bold mb-8">Books</h1>
+      {loading && (
+        <div className="text-center">
+          <span role="img" aria-label="loading">
+            🔄
+          </span>
+        </div>
+      )}
+      {error && (
+        <div className="text-center">
+          <span role="img" aria-label="error">
+            💔
+          </span>
+        </div>
+      )}
+      {data && (
+        <>
+          <div>
+            {data?.books?.list.map((book) => (
+              <div className="border-2 rounded py-4 px-8 mb-2 flex flex-col sm:flex-row sm:justify-between">
+                <h4 className="text-lg">{book.title}</h4>
+                <p className="text-gray-600 text-sm">{book.author.name}</p>
+              </div>
+            ))}
+          </div>
+          <div className="mt-5">
+            {pageNo !== 1 && (
+              <button
+                className="float-left text-center inline-block bg-green-400 hover:bg-green-500 text-white py-1 px-4 rounded transition-colors duration-300 ease-in-out"
+                onClick={() => setPageNo((n) => n - 1)}
+              >
+                PREV
+              </button>
+            )}
+            {data?.books?.hasMore && (
+              <button
+                className="float-right text-center inline-block bg-green-400 hover:bg-green-500 text-white py-1 px-4 rounded transition-colors duration-300 ease-in-out"
+                onClick={() => setPageNo((n) => n + 1)}
+              >
+                NEXT
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
