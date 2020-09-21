f876d64c7c6981069aaae9974b2dee17a650eff3
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-21 23:27:09 +0500

    ⚡ IMPROVE: AuthorsPage with antd

--- src/pages/Authors.js
+++ src/pages/Authors.js
@@ -2,18 +2,18 @@ import { PageHeader } from "antd";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-const Books = () => {
+const Authors = () => {
   const history = useHistory();
   return (
     <>
       <PageHeader
         className="site-page-header"
         onBack={() => history.goBack()}
-        title="Books"
-        subTitle="View / Add all Books here"
+        title="Authors"
+        subTitle="View / Add all Authors here"
       />
     </>
   );
 };
 
-export default Books;
+export default Authors;
