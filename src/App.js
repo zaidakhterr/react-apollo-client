383494c9e86cd674495b371a9bef7b251d58698e
Zaid Akhter <zaidakhter1202@gmail.com>
2020-09-21 22:09:29 +0500

    🔥 DELETE: bye bye tailwind!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Notfound from "./pages/404";
-import Authors from "./pages/Authors";
-import Books from "./pages/Books";
-import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <>
-      <div className="md:container mx-auto px-4 min-h-screen flex flex-col justify-center align-middle">
-        <Switch>
-          <Route exact path="/" children={<Home />} />
-          <Route exact path="/authors" children={<Authors />} />
-          <Route exact path="/books" children={<Books />} />
-          <Route exact path="*" children={<Notfound />} />
-        </Switch>
-      </div>
-    </>
-  );
+  return <>TAILWIND SUCKS (ATLEAST FOR ME)</>;
 };
 
 export default App;
